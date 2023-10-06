68b01b078b5e1c9f3b61029a6ca69a5570cc9c4d
thomas demoncy <demoncy.thomas@gmail.com>
2023-10-06 15:18:54 +0200

    made the grid working a bit by commenting previous css statements that were messing up with the grid from mui

--- src/pages/CardDetailsPage.jsx
+++ src/pages/CardDetailsPage.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { CircularProgress } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Chip from '@mui/material/Chip';
 
 function CardDetails() {
     const { cardId } = useParams(); 
@@ -23,7 +25,36 @@ function CardDetails() {
 
     return isLoading ? (<CircularProgress/ >) :
     ( 
-        <h1>hello</h1>
+        <>
+        <h1>{card.name}</h1>
+        {/* row1 */}
+        {/* Col1 */}
+        <Grid container spacing={2}>  
+            <Grid item xs={4} md={4} sx={{}}>
+               <img src={card.images.large} alt="" style={{width: '250px'}}/>
+            </Grid>
+            <Grid item xs={8} md={8} sx={{}}>
+                <div>
+                <h2>About the card:</h2>
+                    <p>Set Name: <Chip label={card.set.name} variant="outlined" /> </p> 
+                    <p>Series: <Chip label={card.set.series} variant="outlined" />  </p>
+                    <p>Number of cards printed: <Chip label={card.set.printedTotal}variant="outlined" />  </p>
+                    <p>Rarity: <Chip label={card.rarity} variant="outlined" />  </p>
+                </div>
+                <div>
+            <h2>Prices:</h2>
+                <p>Low Price: {card.cardmarket.prices.lowPrice} €</p>
+                <p>Average Price: {card.cardmarket.prices.trendPrice} €</p>
+                <p>30d Average Price: {card.cardmarket.prices.avg30} €</p>
+            </div>
+            </Grid>
+        </Grid>
+        {/* Col2 */}
+
+
+        {/* row2 */}
+
+        </>
      )
 }
 
