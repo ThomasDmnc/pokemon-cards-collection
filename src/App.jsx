3d48487b331ded3546bbb16ffeb38d0b51471429
Eugen Freund <Eugen.Freund@outlook.de>
2023-10-06 12:31:26 +0200

    add react routing dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './pages/HomePage'
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Hallo</h1>
-    {/* <Routes>
-      <Route></Route>
-    </Routes> */}
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+
+      <Route path="*" element={<h1>404 Page</h1>} />
+
+    </Routes>
     </>
   )
 }
