f9e68116d686f39054909af56fdc0cfd874c6269
thomas demoncy <demoncy.thomas@gmail.com>
2023-10-06 17:02:13 +0200

    created navbar compoment and moved some css statements to a container class holding the others compomenents

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,23 @@ import './App.css'
 import HomePage from './pages/HomePage'
 import {Route, Routes} from 'react-router-dom'
 import CardDetails from './pages/CardDetailsPage'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Routes>
+    <Navbar />
+    <div className='ctn'>    
+      <Routes>
       <Route path="/" element={<HomePage />} />
 
       <Route path="cards/:cardId" element={<CardDetails />} />
       <Route path="*" element={<h1>404 Page</h1>} />
 
     </Routes>
+    </div>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (  
+        <>
+        <div className='navbar'>
+            <img src="./src/assets/pokellection.png" alt="" />
+            <p>hello</p>
+        </div>
+        </>
+    );
+}
+
+export default Navbar;
