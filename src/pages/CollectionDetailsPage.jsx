7f30ed90aa284a6905f8a9a4a85b1893998de6dc
thomas demoncy <demoncy.thomas@gmail.com>
2023-10-09 14:10:00 +0200

    made the useParms + call to api to retrieve the data from collection/id

--- src/pages/CollectionDetailsPage.jsx
+++ src/pages/CollectionDetailsPage.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
+
 function CollectionDetails() {
-    return (
+    const { collectionId } = useParams();
+    const [collection, setCollection] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+    const apiUrl =  import.meta.env.VITE_API_URL;
+
+    async function getCollection() {
+        const response = await fetch(`${apiUrl}/collections/${collectionId}`)
+        if (response.ok) {
+            const collectionsApi = await response.json()
+            setCollection(collectionsApi);
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() =>{
+        getCollection();
+    })
+
+    return isLoading ? (<CircularProgress/ >) :  (
         <>
-            <h1>Daft Punk</h1>
+            <h1> {collection.name} </h1>
+            <p>{collection.description}</p>
+            <h4>The collection's card:</h4>
         </>
     );
 }
