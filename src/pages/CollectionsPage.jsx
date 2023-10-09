5ec445a671e1fcb38d830002f8b13835fe54083f
thomas demoncy <demoncy.thomas@gmail.com>
2023-10-09 12:20:00 +0200

    semi commit to send out the code

--- src/pages/CollectionsPage.jsx
+++ src/pages/CollectionsPage.jsx
@@ -1,28 +1,43 @@
 import { useEffect, useState } from "react";
+import Grid from '@mui/material/Grid'
+import { CircularProgress } from "@mui/material"
 
 function CollectionsPage() {
     const [collections, setCollections] = useState();
+    const [isLoading, setIsLoading] = useState(true);
     const apiUrl =  import.meta.env.VITE_API_URL;
 
     async function getAllCollections() {
         const response = await fetch(`${apiUrl}/collections`)
-        console.log(response)
-        if (response.ok){
+
+        if (response.ok) {
             const collectionsApi = await response.json()
-            console.log(collectionsApi)
             setCollections(collectionsApi)
+            setIsLoading(false);
         }
     }
 
     useEffect(() =>{
-        getAllCollections()
+        getAllCollections();
     })
 
-    return (
+
+    //   Object.keys(collections).map(function (key, index) {
+    //     console.log(collections[key].id);
+    // });
+
+    return isLoading ? (<CircularProgress/ >) : (
         <>
-            <h1>Hello</h1>
+            <h1>All the collections:</h1>
+            <ul>
+                {collections.map((collection) => {
+                    <div key={collection.id}>
+                        <p>X</p>
+                    </div>
+                })}
+            </ul>
         </>
-    );
+    )
 }
 
 export default CollectionsPage;
