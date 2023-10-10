6e5d8df2ec84ea23cc497b53c4ef210fb97fe632
Eugen Freund <Eugen.Freund@outlook.de>
2023-10-10 15:33:34 +0200

    add snackBar

--- src/pages/CardDetailsPage.jsx
+++ src/pages/CardDetailsPage.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { CircularProgress, Typography } from '@mui/material';
+import { CircularProgress, Snackbar, Typography } from '@mui/material';
 import './CardDetailsPage.css';
 import Grid from '@mui/material/Grid';
 import Chip from '@mui/material/Chip';
@@ -18,13 +18,13 @@ const style = {
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
-  };
+};
 
 function CardDetails() {
     const { cardId } = useParams();
     const [card, setCard] = useState();
     const [isLoading, setIsLoading] = useState(true);
-    const [open, setOpen] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
     const [collections, setCollections] = useState()
     const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -38,31 +38,34 @@ function CardDetails() {
         } catch (error) {
             console.log(error)
         }
-        setOpen(true)
+        setOpenModal(true)
     };
 
     const addCardToCollection = async (collection) => {
-        event.preventDefault()
-        const payload = {cards: [...collection.cards, cardId] }
-        try {
-            const response = await fetch(`${apiUrl}/collections/${collection.id}`,
-            {
-                method: 'PATCH',
-                body: JSON.stringify(payload),
-                headers: {
-                    'Content-type': 'application/json',
-                },
-            })
-            if (response.ok) {
-                console.log(response)
+        if (collection.cards.includes(cardId)) {
+            setSnackBar({open: true, message: `This Card is alredy in the collection: ${collection.name}`})
+        } else {
+            const payload = { cards: [...collection.cards, cardId] }
+            try {
+                const response = await fetch(`${apiUrl}/collections/${collection.id}`,
+                    {
+                        method: 'PATCH',
+                        body: JSON.stringify(payload),
+                        headers: {
+                            'Content-type': 'application/json',
+                        },
+                    })
+                if (response.ok) {
+                    setSnackBar({open: true, message: `Card successful added to the collection: ${collection.name}`})
+                }
+            } catch (error) {
+                console.log(error)
             }
-        } catch (error) {
-            console.log(error)
         }
-        setOpen(false)
+        setOpenModal(false)
     };
 
-    const handleClose = () => setOpen(false);
+    const handleCloseModal = () => setOpenModal(false);
 
     async function fetchCardDetails() {
         const response = await fetch(`https://api.pokemontcg.io/v2/cards/${cardId}`)
@@ -117,8 +120,8 @@ function CardDetails() {
                     </Grid>
                 </Grid>
                 <Modal
-                    open={open}
-                    onClose={handleClose}
+                    open={openModal}
+                    onClose={handleCloseModal}
                     aria-labelledby="modal-modal-title"
                     aria-describedby="modal-modal-description"
                 >
@@ -128,10 +131,10 @@ function CardDetails() {
                         </Typography>
                         {collections && (<Typography id="modal-modal-description" sx={{ mt: 2 }}>
                             {collections.map((collection) => (
-                                <li key={collection.id}><Button onClick={()=>addCardToCollection(collection)}>{collection.name}</Button></li>
+                                <li key={collection.id}><Button onClick={() => addCardToCollection(collection)}>{collection.name}</Button></li>
                             ))}
-                        </Typography>) }
-                        <Button onClick={handleClose}>No</Button>
+                        </Typography>)}
+                        <Button onClick={handleCloseModal}>No</Button>
                     </Box>
                 </Modal>
                 {/* row2 */}
