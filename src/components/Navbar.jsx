b4355c48eeb8f5d7c0199ddaacb0e7fd00685d1e
thomas demoncy <demoncy.thomas@gmail.com>
2023-10-10 11:46:13 +0200

    fixed the redirections in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,13 @@ function Navbar() {
         <>
         <div className='navbar'>
             <Link to='/'>
-                    <img src="/src/assets/pokellection.png" alt="" />
+                    <img src="./src/assets/pokellection.png" alt="" />
             </Link>
             <div>
-                <Button variant="contained" href="/collections/new" sx={{background: "#3B79C9", border: "4px solid #FFCD05", textTransform: 'capitalize'}}>
+                <Button variant="contained" component={Link} to={`/collections/new`} sx={{background: "#3B79C9", border: "4px solid #FFCD05", textTransform: 'capitalize'}}>
                     Create your collection
                 </Button>
-                <Button href="/collections"  sx={{ textTransform: 'capitalize'}}>
+                <Button component={Link} to={`/collections`} sx={{ textTransform: 'capitalize'}}>
                     The collections
                 </Button>
             </div>
