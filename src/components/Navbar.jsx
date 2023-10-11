037ce25e5e46f37d55eea2fe6a353ca9c70f8a81
thomas demoncy <demoncy.thomas@gmail.com>
2023-10-11 12:02:14 +0200

    added the search bar feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
                 <Button variant="contained" component={Link} to={`/collections/new`} sx={{background: "#3B79C9", border: "4px solid #FFCD05", textTransform: 'capitalize'}}>
                     Create your collection
                 </Button>
-                <Button component={Link} to={`/collections`} sx={{ textTransform: 'capitalize'}}>
+                <Button component={Link} to={`/collections`} sx={{marginLeft: 2, textTransform: 'capitalize'}}>
                     The collections
                 </Button>
             </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
-import { Card, CardActionArea, CardMedia, CircularProgress, Pagination } from "@mui/material"
+import { Box, Card, CardActionArea, CardMedia, CircularProgress, Pagination, TextField } from "@mui/material"
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
 
 function HomePage() {
     const [cardsPerPage, setCardsPerPage] = useState(9);
-    const [cards, setCards] = useState()
+    const [cards, setCards] = useState();
     const [currentPage, setCurrentPage] = useState(1);
     const [totalCards, setTotalCards] = useState(0);
+    const [search, setSearch] = useState('');
 
     const handlePageChange = (event, newPage) => {
         getCards(newPage)
@@ -16,12 +17,18 @@ function HomePage() {
 
 
     const getCards = async (loadPage) => {
-        const response = await fetch(`https://api.pokemontcg.io/v2/cards?page=${loadPage}&pageSize=${cardsPerPage}`)
+        const response = await fetch(`https://api.pokemontcg.io/v2/cards?${search}page=${loadPage}&pageSize=${cardsPerPage}`)
         if (response.ok) {
             const cardsAPI = await response.json()
             setTotalCards(cardsAPI.totalCount)
             setCards(cardsAPI.data)
         }
+    }
+
+    const handleSearchChange = event => {
+        setSearch("&q=name:"+event.target.value+"*&")
+        console.log(search)
+        getCards(currentPage)
     }
 
     useEffect(() => {
@@ -30,7 +37,12 @@ function HomePage() {
 
     return cards ? (
         <div>
-            <Grid2 container spacing={5}>
+            <h1>Start your Pokéllection</h1>
+            <h4>Looking for a specific Pokémon:</h4>
+            <div className="searchBar" style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <TextField onChange={handleSearchChange} name="search" type="text" id="filled-basic" label="Name of the Pokémon you're looking for" variant="filled" sx={{width: '100%', m: 2}}/>
+            </div>
+            <Grid2 container spacing={5} sx={{mt: 2, mb: 2}}>
                 {cards.map((card) => (
                     <Grid2 key={card.id} item="true" xs={12} sm={6} md={4}>
                         <Card sx={{ maxWidth: 245 }}>
@@ -46,12 +58,14 @@ function HomePage() {
                     </Grid2>
                 ))}
             </Grid2>
+            <Box sx={{display: 'flex', alignItems: 'center', justifyContent:'center'}}>
             <Pagination
                 variant="outlined"
                 shape="rounded"
                 count={Math.ceil(totalCards / cardsPerPage)}
                 page={currentPage}
                 onChange={handlePageChange}/>
+            </Box>
         </div>
     ) :
         (<CircularProgress />);
